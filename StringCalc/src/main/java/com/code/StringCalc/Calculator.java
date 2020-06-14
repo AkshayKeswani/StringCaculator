f0deffff5b216bda7097d6022b7c230e4a778241
AKSHAY <AKSHAY@DESKTOP-4U5ES51>
2020-06-14 20:06:34 +0530

    One number in String

--- StringCalc/src/main/java/com/code/StringCalc/Calculator.java
+++ StringCalc/src/main/java/com/code/StringCalc/Calculator.java
@@ -3,10 +3,18 @@ package com.code.StringCalc;
 public class Calculator {
 
 	public int StringCalculator(String inputOne) {
+		
+		String[] element=inputOne.split(",");
 		if(inputOne=="") {
 			return 0;
 		}
 		
-		return 90;
+		else if(element.length==1) {
+			String s2=element[0];
+			return Integer.parseInt(s2);
+		}
+		
+		return 1;
+		
 	}
 }
--- StringCalc/src/test/java/com/code/StringCalc/CalculatorTest.java
+++ StringCalc/src/test/java/com/code/StringCalc/CalculatorTest.java
@@ -6,10 +6,18 @@ import org.junit.Test;
 
 public class CalculatorTest {
 
+	Calculator cal=new Calculator();
+	
 	@Test
-	public void test() {
-		Calculator cal=new Calculator();
-		assertEquals(0,cal.StringCalculator(""));
+	public void emptyString() {
+	assertEquals(0,cal.StringCalculator(""));
 	}
 
+	@Test
+	public void oneNumber() {
+	String s1="2";
+	Integer.parseInt(s1);
+	assertEquals(Integer.parseInt(s1),cal.StringCalculator(s1));
+	}
+	
 }
